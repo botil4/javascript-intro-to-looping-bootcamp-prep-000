b5b83a2041152ec4965d451ed6738914cb2cec4f
Travis Clarkson <tk1667@gmail.com>
2018-02-05 01:45:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 var array = []
 
 function forLoop(array) {
-for (var i = 0; i < 25; i++) {
-  if (i === "1") {
-    array[i] = "I am 1 strange loop."
-  } else {
-    array[i] = `I am ${i} strange loops.`
+  for (var i = 0; i < 25; i++) {
+    if (i === "1") {
+      array[i] = "I am 1 strange loop."
+    } else {
+      array[i] = `I am ${i} strange loops.`
+    }
   }
-}
-return array
+  return array
 }
