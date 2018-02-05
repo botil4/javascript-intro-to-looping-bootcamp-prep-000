a3b6c4a841e35f794142f433bb83d1d15f5d1371
Travis Clarkson <tk1667@gmail.com>
2018-02-05 01:40:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var array = []
 
 for (var i = 0; i < 25; i++) {
-  if (i === 1) {
+  if (i === "1") {
     array[parseInt(i)] = "I am 1 strange loop."
   } else {
     array[parseInt(i)] = `I am ${i} strange loops.`
